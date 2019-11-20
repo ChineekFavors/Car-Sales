fb0205044b6568bd52a31b681db49cfe0d882cb0
Chineek Favors <chineekfavors01@gmail.com>
2019-11-20 00:28:52 -0800

    refactored AddedFeatures.js to connect to redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
       <figure className="image is-128x128">
         <img src={props.image} alt={props.name} />
       </figure>
-      <h2>{props.car.name}</h2>
+      <h2>{props.name}</h2>
       <p>Amount: ${props.price}</p>
     </>
   );
